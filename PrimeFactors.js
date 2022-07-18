514a00017b8c645ec5d3438bb12658e155bf0f5a
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-07-19 01:00:19 +0530

    Day 38 Js Pratice Problem

---
+++ PrimeFactors.js
@@ -0,0 +1,21 @@
+/** Find prime factors  */
+import promptSync from 'prompt-sync';
+const prompts = require("prompt-sync")();
+let number = parseInt(prompts("Enter a number to find prime factors : "));
+let primeNumber = [];
+function primeFactors() {
+    for (i = 2; i <= number; i++) {
+        if (number % i == 0) {
+            for (j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                }
+            }
+            isPrime = true;
+            primeNumber.push(i);
+            number = number / i;
+        }
+    }
+    console.log(primeNumber);
+}
+primeFactors();
